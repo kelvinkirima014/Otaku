c0cc922118ce775d5c5bba6166dd2bea90372a62
kelvinkirima014 <kelvinkirima2@gmail.com>
2022-10-13 08:48:35 +0300

    add account validation logic

--- programs/otaku/src/lib.rs
+++ programs/otaku/src/lib.rs
@@ -7,9 +7,25 @@ pub mod otaku {
     use super::*;
 
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        //Get a reference to the account
+        let base_account = &mut ctx.accounts.base_account;
+        //initialize total_gifs
+        base_account.total_gifs = 0;
         Ok(())
     }
-}
+} 
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+    #[account(init, payer = signer, space = 9000 )]
+    pub base_account: Account<'info, BaseAccount>,
+    #[account(mut)]
+    pub signer: Signer<'info>, 
+    pub system_program: Program<'info, System>,
+
+}
+
+#[account]
+pub struct BaseAccount {
+    pub total_gifs: u64,
+}
