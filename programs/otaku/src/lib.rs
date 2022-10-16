1134c04f64a367dc8d69fbf0c8048ccd472c1dc4
kelvinkirima014 <kelvinkirima2@gmail.com>
2022-10-16 21:38:55 +0300

    import anchor packages

--- programs/otaku/src/lib.rs
+++ programs/otaku/src/lib.rs
@@ -1,6 +1,6 @@
 use anchor_lang::prelude::*;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("2amGSCnMV6RzJothNXnskJLcJoEykXNfgNssFbtwRkVL");
 
 #[program]
 pub mod otaku {
